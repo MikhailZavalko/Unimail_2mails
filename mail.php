b60ff4b0d28b158bdd99a6a43a2b35565ad4fdb6
Mikhail Zavalko <mikhailzavalko@gmail.com>
2017-08-12 03:41:55 +1000

    create mail.php
    First commit

---
+++ mail.php
@@ -0,0 +1,43 @@
+<?php
+// несколько получателей
+$to  = 'mikhailzavalko@gmail.com' . ', ';  // обратите внимание на запятую
+$to .= 'petrov0389@gmail.com';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+//Script Foreach
+$c = true;
+if ( $method === 'POST' ) {
+
+	$project_name = trim($_POST["project_name"]);
+	$admin_email  = trim($_POST["admin_email"]);
+	$admin_email  = trim($_POST["admin_email_2"]);
+	$form_subject = trim($_POST["form_subject"]);
+
+	foreach ( $_POST as $key => $value ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "admin_email_2" && $key != "form_subject" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+		</tr>
+		";
+	}
+}
+} 
+
+$message = "<table style='width: 100%;'>$message</table>";
+
+function adopt($text) {
+	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+}
+
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+'Reply-To: '.$admin_email.'' . PHP_EOL;
+
+
+// Отправляем
+mail($to, $subject, $message, $headers);
+?>
